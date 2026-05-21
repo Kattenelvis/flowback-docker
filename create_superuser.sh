e305307f0c082e0e66aeb50cc8ad9b014aa5e342
emil <53380638+Kattenelvis@users.noreply.github.com>
2026-05-21 20:21:46 +0200

    it

--- create_superuser.sh
+++ create_superuser.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Create Django superuser in the flowback-backend Docker container
 
-docker exec \
+docker exec -it \
   flowback-docker-flowback-backend-1 \
   python manage.py createsuperuser
